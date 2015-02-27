bb31a9b2f4b250f2ab28f083d6558c7e87e94586
sunzhyng <szy@10.0.3.25>
2015-02-27 17:50:59 +0800

    sid

--- src/main/java/com/forum/po/User.java
+++ src/main/java/com/forum/po/User.java
@@ -3,6 +3,10 @@ package com.forum.po;
 import java.io.Serializable;
 
 public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
 	private Integer age;
